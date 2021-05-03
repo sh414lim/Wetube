52bd648571830bbbad05f8f451a170abc52eee2a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-03 10:24:46 +0900

    .

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ const handleListening=()=>{
 console.log(`Listening on: http://localhost:${PORT}`)
 }
 
-const  handleHome=(req,res)=>{
-    console.log(req)
-    res.send('Hello from House')
-}
-
-const handleProfile=(req,res)=>{
-res.send("You are on y profile")
-}
-
-app.get("/",handleHome);
 
 app.get('/profile',handleProfile)
 
