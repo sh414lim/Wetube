58c0a07520ea05d120ccfcbb26ae5adb94d3a8ca
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-08 17:26:24 +0900

    .

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,12 +1,8 @@
 export const trending = (req,res)=>res.render("home");
 
-export const see=(req,res)=>{
-    console.log(req.params);
-    return res.render("watch")
-};
-export const edit=(req,res)=>{
-return res.render("edit");
-};
+export const see=(req,res)=> res.render("watch");
+export const edit=(req,res)=> res.render("edit");
+
 export const search=(req,res)=>res.send("Search");
 export const remove=(req,res)=>{
    return res.send("Veideo Remove")
