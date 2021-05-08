1628731311f8a1d5ed06caff71e92c48d9d3b0a0
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-08 17:28:30 +0900

    .

--- src/router/VideoRouter.js
+++ src/router/VideoRouter.js
@@ -4,7 +4,7 @@ import {see,edit,remove,upload} from "../controllers/videoControllers";
 const videoRouter=express.Router();
 
 
-videoRouter.get("/upload",upload)
+videoRouter.get("/upload",upload);
 videoRouter.get("/:id",see);
 videoRouter.get("/:id/edit",edit);
 videoRouter.get("/:id/remove",remove);
